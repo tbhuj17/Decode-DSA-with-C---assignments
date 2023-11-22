da8fb4b2cb309ae5772abedc600a8279775e6168
tbhuj17 <tbhujade17@gmail.com>
2023-11-22 19:49:41 +0530

    completed with selection and istertion sort

---
+++ Week_9_Assignments/C++ Assignments  Selection and insertion sorting  Week 9/Question_4.cpp
@@ -0,0 +1,13 @@
+// Given an array of digits (values are from 0 to 9), the task is to find the minimum possible sum of
+// two numbers formed from digits of the array. Please note that all digits of the given array must be
+// used to form the two numbers.
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+int main()
+{
+    
+}
